717976a075182dcdb34e6e39b6391f0030c57b0e
dev gurjar <125337294+devgurjar-coder@users.noreply.github.com>
2024-02-02 23:40:18 -0800

    Update ShowVideo.jsx

--- src/Components/ShowVideo/ShowVideo.jsx
+++ src/Components/ShowVideo/ShowVideo.jsx
@@ -8,7 +8,7 @@ function ShowVideo({vid}) {
     <>
      <Link to={`/videopage/${vid?._id}`}>
         <video 
-        src={`http://localhost:5500/${vid.filePath}`}
+        src={`https://mern-stack-youtube-backend.onrender.com/${vid.filePath}`}
         // src={`https://youtubeclone5031.herokuapp.com/${vid.filePath}`}
         className="video_ShowVideo"
         />
